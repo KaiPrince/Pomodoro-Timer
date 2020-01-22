fd60a23b7f1635ebf33757515ddda8763eb186fc
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2020-01-22 12:28:02 -0500

    Clean App Bar.

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-
-        <v-btn text to="/">Home</v-btn>
-        <v-divider inset vertical></v-divider>
-        <v-btn text to="/about">About</v-btn>
-        <v-divider inset vertical></v-divider>
-        <v-btn text to="/pomodoro">Pomodoro</v-btn>
+        <v-toolbar-title>Pomodoro</v-toolbar-title>
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
