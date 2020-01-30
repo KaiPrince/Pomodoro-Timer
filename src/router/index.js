abe6f6e830e5b63f869e3a553dbef3fd9bb127aa
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2020-01-30 14:18:53 -0500

    Add Settings page.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Pomodoro from "../views/Pomodoro.vue";
+import Settings from "../views/Settings.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "home",
     component: Pomodoro
+  },
+  {
+    path: "/settings",
+    name: "settings",
+    component: Settings
   }
 ];
 
---
+++ src/views/Settings.vue
@@ -0,0 +1,23 @@
+<!-- This is the view for the PomodoroSettings Component -->
+
+<template>
+  <v-container class="fill-height">
+    <v-row
+      ><v-col>
+        <v-row justify="center" align="center"> <PomodoroSettings /></v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import PomodoroSettings from "@/components/PomodoroSettings.vue";
+export default {
+  name: "PomodoroView",
+  components: {
+    PomodoroSettings
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
